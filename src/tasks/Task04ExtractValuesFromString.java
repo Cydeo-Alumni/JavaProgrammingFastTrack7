0a08967959eecc10caa764617c035ab8963022f7
Murodil <murodil@cydeo.com>
2024-03-06 14:29:16 -0500

    extract string values with junit assertion

--- src/tasks/Task04ExtractValuesFromString.java
+++ src/tasks/Task04ExtractValuesFromString.java
@@ -1,5 +1,9 @@
 package tasks;
 
+import org.junit.jupiter.api.Assertions;
+
+import java.text.DecimalFormat;
+
 public class Task04ExtractValuesFromString {
     public static void main(String[] args) {
         String str = "40% off Deal $59.99 List Price: $99.99";
@@ -12,6 +16,16 @@ public class Task04ExtractValuesFromString {
 
         double listPrice = Double.parseDouble(str.substring(str.lastIndexOf("$")+1));
         System.out.println("listPrice = " + listPrice);
+                                                //calculate discount amount
+        double expPriceAfterDiscount = listPrice - (listPrice * percentage/100.0);
+        //59.99399999999999 need to convert to 59.99, only 2 decimal places
+        DecimalFormat decimalFormat = new DecimalFormat("##.00");
+        //format and assign back to variable.
+        expPriceAfterDiscount = Double.parseDouble(decimalFormat.format(expPriceAfterDiscount));
 
+        System.out.println("expPriceAfterDiscount = " + expPriceAfterDiscount);
+
+        Assertions.assertEquals(expPriceAfterDiscount, dealPrice);
+        
     }
 }
